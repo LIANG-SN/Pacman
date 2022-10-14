c0a4bcd963b9f544ffaad39f17cd9b3019734a01
sliang92 <sliang92@gatech.edu>
2022-10-13 21:39:44 -0400

    revert "Implemented player's name, life , and round. With a simple enemy traveling path."
    This reverts commit 5ce1fb57ab6388da20197ca9fd4ff305b2a0a924.

--- src/main/java/cs2340/group65/pacman/Controller.java
+++ src/main/java/cs2340/group65/pacman/Controller.java
@@ -2,35 +2,11 @@ package cs2340.group65.pacman;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
 
-public class Controller{
-
-    private VBox gameScreen;
-    private HBox playerInfoBar;
-    private Label playerName;
-    private Label playerLife;
-    private Label round;
-
-
-    public Controller () {
-        MainGameScreen game = new MainGameScreen(640, 280);
-        gameScreen = new VBox();
-        playerInfoBar = new HBox(20);
-        playerName = new Label("Player's name");
-        playerLife = new Label("Life: 10");
-        round = new Label("Round: One");
-        playerInfoBar.setAlignment(Pos.TOP_LEFT);
-        playerInfoBar.getChildren().addAll(playerName, playerLife, round);
-        gameScreen.getChildren().addAll(playerInfoBar, game);
-        game.play();
-    }
+public class Controller {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot(gameScreen);
+        App.setRoot("secondary");
     }
 }
